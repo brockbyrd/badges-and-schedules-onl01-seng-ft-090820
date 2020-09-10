97569a25269ae8cda78a7bd7f65a81e12da1ac8e
Brock Byrd <brockbyrdd@gmail.com>
2020-09-10 17:22:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(badges)
   badges.map{|attendee| badge_maker(attendee)}
 end
 
